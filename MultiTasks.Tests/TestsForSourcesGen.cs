19c3bc3ee0bf9f400330c868651abaf6acdda778
Fabrice Ferreira Leal <fabrice.leal.ch@gmail.com>
2012-12-03 21:28:53 +0000

    tests now output to log

--- MultiTasks.Tests/TestsForSourcesGen.cs
+++ MultiTasks.Tests/TestsForSourcesGen.cs
@@ -3,6 +3,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MultiTasks.RT;
 using System;
+using System.IO;
 
 namespace MultiTasks.Tests
 {
@@ -12,23 +13,55 @@ namespace MultiTasks.Tests
     {
 		
 		[TestMethod]
+		public void Eval01Helloworld()
+		{
+			try 
+			{				
+				// Read file content (as embedded resource)
+				var src = Utils.ReadSourceFileContent("01_helloworld.mt");
+			
+				using(Stream str = new FileStream("01_helloworld.mt.log", FileMode.OpenOrCreate, FileAccess.Write))
+				{
+					// Compile
+					var res = MtCompiler.CreateScriptApp(str).Evaluate(src) as MtResult;
+
+					// Tests ...
+					Assert.IsNotNull(res);
+
+					// Wait for end
+					res.GetValueSync((o) => { });
+
+					// ... More tests
+				}				
+			} 
+			catch(Exception e)
+			{
+				Assert.Fail(e.Message);
+			}
+		}
+
+		
+		[TestMethod]
 		public void Eval01HelloWorld()
 		{
 			try 
-			{
+			{				
 				// Read file content (as embedded resource)
 				var src = Utils.ReadSourceFileContent("01_hello_world.mt");
 			
-				// Compile
-				var res = MtCompiler.CreateScriptApp().Evaluate(src) as MtResult;
+				using(Stream str = new FileStream("01_hello_world.mt.log", FileMode.OpenOrCreate, FileAccess.Write))
+				{
+					// Compile
+					var res = MtCompiler.CreateScriptApp(str).Evaluate(src) as MtResult;
 
-				// Tests ...
-				Assert.IsNotNull(res);
+					// Tests ...
+					Assert.IsNotNull(res);
 
-				// Wait for end
-				res.GetValueSync((o) => { });
+					// Wait for end
+					res.GetValueSync((o) => { });
 
-				// ... More tests
+					// ... More tests
+				}				
 			} 
 			catch(Exception e)
 			{
@@ -41,20 +74,23 @@ namespace MultiTasks.Tests
 		public void Eval02HelloWithFork()
 		{
 			try 
-			{
+			{				
 				// Read file content (as embedded resource)
 				var src = Utils.ReadSourceFileContent("02_hello_with_fork.mt");
 			
-				// Compile
-				var res = MtCompiler.CreateScriptApp().Evaluate(src) as MtResult;
+				using(Stream str = new FileStream("02_hello_with_fork.mt.log", FileMode.OpenOrCreate, FileAccess.Write))
+				{
+					// Compile
+					var res = MtCompiler.CreateScriptApp(str).Evaluate(src) as MtResult;
 
-				// Tests ...
-				Assert.IsNotNull(res);
+					// Tests ...
+					Assert.IsNotNull(res);
 
-				// Wait for end
-				res.GetValueSync((o) => { });
+					// Wait for end
+					res.GetValueSync((o) => { });
 
-				// ... More tests
+					// ... More tests
+				}				
 			} 
 			catch(Exception e)
 			{
@@ -67,20 +103,52 @@ namespace MultiTasks.Tests
 		public void Eval03Binds()
 		{
 			try 
-			{
+			{				
 				// Read file content (as embedded resource)
 				var src = Utils.ReadSourceFileContent("03_binds.mt");
 			
-				// Compile
-				var res = MtCompiler.CreateScriptApp().Evaluate(src) as MtResult;
+				using(Stream str = new FileStream("03_binds.mt.log", FileMode.OpenOrCreate, FileAccess.Write))
+				{
+					// Compile
+					var res = MtCompiler.CreateScriptApp(str).Evaluate(src) as MtResult;
 
-				// Tests ...
-				Assert.IsNotNull(res);
+					// Tests ...
+					Assert.IsNotNull(res);
 
-				// Wait for end
-				res.GetValueSync((o) => { });
+					// Wait for end
+					res.GetValueSync((o) => { });
 
-				// ... More tests
+					// ... More tests
+				}				
+			} 
+			catch(Exception e)
+			{
+				Assert.Fail(e.Message);
+			}
+		}
+
+		
+		[TestMethod]
+		public void Eval04PrintAndBindPrint()
+		{
+			try 
+			{				
+				// Read file content (as embedded resource)
+				var src = Utils.ReadSourceFileContent("04_print_and_bind_print.mt");
+			
+				using(Stream str = new FileStream("04_print_and_bind_print.mt.log", FileMode.OpenOrCreate, FileAccess.Write))
+				{
+					// Compile
+					var res = MtCompiler.CreateScriptApp(str).Evaluate(src) as MtResult;
+
+					// Tests ...
+					Assert.IsNotNull(res);
+
+					// Wait for end
+					res.GetValueSync((o) => { });
+
+					// ... More tests
+				}				
 			} 
 			catch(Exception e)
 			{
